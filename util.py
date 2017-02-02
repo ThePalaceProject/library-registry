f7512bea6224af38de99aeae55540c59dc784f94
Leonard Richardson <leonardr@segfault.org>
2017-02-02 13:01:53 -0500

    Removed unnecesary cast.

--- util.py
+++ util.py
@@ -1,6 +1,5 @@
 from nose.tools import set_trace
 from sqlalchemy import func
-from sqlalchemy.sql.expression import cast
 from geoalchemy2 import Geometry
 
 class GeometryUtility(object):
@@ -12,4 +11,4 @@ class GeometryUtility(object):
         """
         geometry = func.ST_GeomFromGeoJSON(geojson)
         geometry = func.ST_SetSRID(geometry, 4326)
-        return cast(geometry, Geometry)
+        return geometry
