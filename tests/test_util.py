4505f26343e39b87409556389d0f6e8bc33ba99c
Leonard Richardson <leonardr@segfault.org>
2017-02-03 14:47:37 -0500

    Added code that turns an IP address into a geometric point.

---
+++ tests/test_util.py
@@ -0,0 +1,16 @@
+from nose.tools import (
+    set_trace,
+    eq_,
+)
+from util import GeometryUtility
+
+class TestGeometryUtility(object):
+
+    def test_point(self):
+        point = GeometryUtility.point("80", "-4")
+        eq_('SRID=4326;POINT (-4 80)', point)
+        
+    def test_point_from_ip(self):
+        point = GeometryUtility.point_from_ip("65.88.88.124")
+        eq_('SRID=4326;POINT (-73.9813 40.7769)', point)
+        
