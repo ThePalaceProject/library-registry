58b6a779cdc1da1dac73bb26c1016299308b7252
Nick Ballenger <nballeng@gmail.com>
2021-03-22 16:20:18 -0400

    more test improvements

--- library_registry/util/http.py
+++ library_registry/util/http.py
@@ -18,47 +18,43 @@ INTEGRATION_ERROR = pd(
 
 
 class IntegrationException(Exception):
-    """An exception that happens when the site's connection to a
-    third-party service is broken.
+    """
+    An exception that happens when the site's connection to a third-party service is broken.
 
-    This may be because communication failed
-    (RemoteIntegrationException), or because local configuration is
+    This may be because communication failed (RemoteIntegrationException), or because local configuration is
     missing or obviously wrong (CannotLoadConfiguration).
     """
 
     def __init__(self, message, debug_message=None):
-        """Constructor.
+        """
+        Constructor.
 
-        :param message: The normal message passed to any Exception
-        constructor.
+        :param message: The normal message passed to any Exception constructor.
 
-        :param debug_message: An extra human-readable explanation of the
-        problem, shown to admins but not to patrons. This may include
-        instructions on what bits of the integration configuration might need
+        :param debug_message: An extra human-readable explanation of the problem, shown to admins but not
+        to patrons. This may include instructions on what bits of the integration configuration might need
         to be changed.
 
-        For example, an API key might be wrong, or the API key might
-        be correct but the API provider might not have granted that
-        key enough permissions.
+        For example, an API key might be wrong, or the API key might be correct but the API provider might
+        not have granted that key enough permissions.
         """
         super(IntegrationException, self).__init__(message)
         self.debug_message = debug_message
 
 
 class RemoteIntegrationException(IntegrationException):
-    """An exception that happens when we try and fail to communicate
-    with a third-party service over HTTP.
-    """
+    """An exception that happens when we try and fail to communicate with a third-party service over HTTP"""
 
     title = lgt("Failure contacting external service")
     detail = lgt("The server tried to access %(service)s but the third-party service experienced an error.")
     internal_message = "Error accessing %s: %s"
 
     def __init__(self, url_or_service, message, debug_message=None):
-        """Indicate that a remote integration has failed.
+        """
+        Indicate that a remote integration has failed.
 
-        `param url_or_service` The name of the service that failed
-           (e.g. "Overdrive"), or the specific URL that had the problem.
+        :param url_or_service: Name of the service that failed (ex: "Overdrive"), or the specific URL that
+            had the problem.
         """
         if (url_or_service and any(url_or_service.startswith(x) for x in ('http:', 'https:'))):
             self.url = url_or_service
@@ -91,7 +87,7 @@ class RemoteIntegrationException(IntegrationException):
 
 
 class BadResponseException(RemoteIntegrationException):
-    """The request seemingly went okay, but we got a bad response."""
+    """The request seemingly went okay, but we got a bad response"""
     title = lgt("Bad response")
     detail = lgt("The server made a request to %(service)s, and got an unexpected or invalid response.")
     internal_message = "Bad response from %s: %s"
@@ -99,14 +95,14 @@ class BadResponseException(RemoteIntegrationException):
     BAD_STATUS_CODE_MESSAGE = "Got status code %s from external server, cannot continue."
 
     def __init__(self, url_or_service, message, debug_message=None, status_code=None):
-        """Indicate that a remote integration has failed.
+        """
+        Indicate that a remote integration has failed.
 
-        `param url_or_service` The name of the service that failed
-           (e.g. "Overdrive"), or the specific URL that had the problem.
+        :param url_or_service: The name of the service that failed (e.g. "Overdrive"), or the specific URL
+            that had the problem.
         """
         super(BadResponseException, self).__init__(url_or_service, message, debug_message)
-        # to be set to 500, etc.
-        self.status_code = status_code
+        self.status_code = status_code      # to be set to 500, etc.
 
     def document_debug_message(self, debug=True):
         if debug:
@@ -118,51 +114,32 @@ class BadResponseException(RemoteIntegrationException):
 
     @classmethod
     def from_response(cls, url, message, response):
-        """Helper method to turn a `requests` Response object into
-        a BadResponseException.
-        """
+        """Helper method to turn a `requests` Response object into a BadResponseException"""
         if isinstance(response, tuple):
-            # The response has been unrolled into a (status_code,
-            # headers, body) 3-tuple.
-            status_code, headers, content = response
+            (status_code, headers, content) = response
         else:
             status_code = response.status_code
             content = response.content
-        return BadResponseException(
-            url, message,
-            status_code=status_code,
-            debug_message="Status code: %s\nContent: %s" % (
-                status_code,
-                content,
-            )
-        )
+
+        return BadResponseException(url, message, status_code=status_code,
+                                    debug_message=f"Status code: {status_code}\nContent: {content}")
 
     @classmethod
     def bad_status_code(cls, url, response):
-        """The response is bad because the status code is wrong."""
+        """The response is bad because the status code is wrong"""
         message = cls.BAD_STATUS_CODE_MESSAGE % response.status_code
-        return cls.from_response(
-            url,
-            message,
-            response,
-        )
+        return cls.from_response(url, message, response)
 
 
-class RequestNetworkException(RemoteIntegrationException,
-                              requests.exceptions.RequestException):
-    """An exception from the requests module that can be represented as
-    a problem detail document.
-    """
+class RequestNetworkException(RemoteIntegrationException, requests.exceptions.RequestException):
+    """An exception from the requests module that can be represented as a problem detail document"""
     title = lgt("Network failure contacting third-party service")
     detail = lgt("The server experienced a network error while contacting %(service)s.")
     internal_message = "Network error contacting %s: %s"
 
 
 class RequestTimedOut(RequestNetworkException, requests.exceptions.Timeout):
-    """A timeout exception that can be represented as a problem
-    detail document.
-    """
-
+    """A timeout exception that can be represented as a problem detail document"""
     title = lgt("Timeout")
     detail = lgt("The server made a request to %(service)s, and that request timed out.")
     internal_message = "Timeout accessing %s: %s"
@@ -170,7 +147,6 @@ class RequestTimedOut(RequestNetworkException, requests.exceptions.Timeout):
 
 class HTTP:
     """A helper for the `requests` module."""
-
     @classmethod
     def get_with_timeout(cls, url, *args, **kwargs):
         """Make a GET request with timeout handling."""
@@ -193,26 +169,21 @@ class HTTP:
         """Call requests.request and turn a timeout into a RequestTimedOut
         exception.
         """
-        return cls._request_with_timeout(
-            url, requests.request, http_method, *args, **kwargs
-        )
+        return cls._request_with_timeout(url, requests.request, http_method, *args, **kwargs)
 
     @classmethod
     def _request_with_timeout(cls, url, make_request_with, *args, **kwargs):
-        """Call some kind of method and turn a timeout into a RequestTimedOut
-        exception.
+        """
+        Call some kind of method and turn a timeout into a RequestTimedOut exception.
 
         The core of `request_with_timeout` made easy to test.
 
         :param url: Make the request to this URL.
-        :param make_request_with: A function that actually makes the
-            HTTP request.
+        :param make_request_with: A function that actually makes the HTTP request.
         :param args: Positional arguments for the request function.
         :param kwargs: Keyword arguments for the request function.
         """
-        process_response_with = kwargs.pop(
-            'process_response_with', cls._process_response
-        )
+        process_response_with = kwargs.pop('process_response_with', cls._process_response)
         allowed_response_codes = kwargs.pop('allowed_response_codes', [])
         disallowed_response_codes = kwargs.pop('disallowed_response_codes', [])
         verbose = kwargs.pop('verbose', False)
@@ -220,8 +191,8 @@ class HTTP:
         if 'timeout' not in kwargs:
             kwargs['timeout'] = 20
 
-        # Unicode data can't be sent over the wire. Convert it
-        # to UTF-8.
+        # Unicode data can't be sent over the wire. Convert it to UTF-8.
+        # TODO: Can this be removed or simplified under Py3?
         if 'data' in kwargs and isinstance(kwargs['data'], str):
             kwargs['data'] = kwargs.get('data').encode("utf8")
         if 'headers' in kwargs:
@@ -239,42 +210,32 @@ class HTTP:
             if verbose:
                 logging.info("Sending %s request to %s: kwargs %r", url, kwargs)
             if len(args) == 1:
-                # requests.request takes two positional arguments,
-                # an HTTP method and a URL. In most cases, the URL
-                # gets added on here. But if you do pass in both
-                # arguments, it will still work.
+                # requests.request takes two positional arguments, an HTTP method and a URL. In most cases, the URL
+                # gets added on here. But if you do pass in both arguments, it will still work.
                 args = args + (url,)
             response = make_request_with(*args, **kwargs)
             if verbose:
-                logging.info(
-                    "Response from %s: %s %r %r",
-                    url, response.status_code, response.headers,
-                    response.content
-                )
+                logging.info(f"Response from {url}: {response.status_code} {response.headers} {response.content}")
         except requests.exceptions.Timeout as e:
-            # Wrap the requests-specific Timeout exception
-            # in a generic RequestTimedOut exception.
+            # Wrap the requests-specific Timeout exception in a generic RequestTimedOut exception.
             raise RequestTimedOut(url, str(e))
         except requests.exceptions.RequestException as e:
-            # Wrap all other requests-specific exceptions in
-            # a generic RequestNetworkException.
+            # Wrap all other requests-specific exceptions in a generic RequestNetworkException.
             raise RequestNetworkException(url, str(e))
 
-        return process_response_with(
-            url, response, allowed_response_codes, disallowed_response_codes
-        )
+        return process_response_with(url, response, allowed_response_codes, disallowed_response_codes)
 
     @classmethod
     def _process_response(cls, url, response, allowed_response_codes=None,
                           disallowed_response_codes=None):
-        """Raise a RequestNetworkException if the response code indicates a
-        server-side failure, or behavior so unpredictable that we can't
-        continue.
+        """
+        Raise a RequestNetworkException if the response code indicates a server-side failure, or behavior
+        so unpredictable that we can't continue.
 
-        :param allowed_response_codes If passed, then only the responses with
+        :param allowed_response_codes: If passed, then only the responses with
             http status codes in this list are processed.  The rest generate
             BadResponseExceptions.
-        :param disallowed_response_codes The values passed are added to 5xx, as
+        :param disallowed_response_codes: The values passed are added to 5xx, as
             http status codes that would generate BadResponseExceptions.
         """
         if allowed_response_codes:
@@ -291,31 +252,21 @@ class HTTP:
         series = cls.series(code)
         code = str(code)
 
-        if allowed_response_codes and (
-                code in allowed_response_codes
-                or series in allowed_response_codes
-        ):
-            # The code or series has been explicitly allowed. Allow
-            # the request to be processed.
+        if allowed_response_codes and (code in allowed_response_codes or series in allowed_response_codes):
+            # The code or series has been explicitly allowed. Allow the request to be processed.
             return response
 
         error_message = None
         if (series == '5xx' or code in disallowed_response_codes or series in disallowed_response_codes):
             # Unless explicitly allowed, the 5xx series always results in an exception.
             error_message = BadResponseException.BAD_STATUS_CODE_MESSAGE
-        elif (allowed_response_codes and not (
-                code in allowed_response_codes
-                or series in allowed_response_codes
-        )):
+        elif (allowed_response_codes and not (code in allowed_response_codes or series in allowed_response_codes)):
             error_message = status_code_not_in_allowed
 
         if error_message:
-            raise BadResponseException(
-                url,
-                error_message % code,
-                status_code=code,
-                debug_message="Response content: %s" % response.content
-            )
+            raise BadResponseException(url, error_message % code, status_code=code,
+                                       debug_message="Response content: %s" % response.content)
+
         return response
 
     @classmethod
--- tests/test_util.py
+++ tests/test_util.py
@@ -1,3 +1,5 @@
+import pytest
+
 from library_registry.util import GeometryUtility
 
 
@@ -9,26 +11,35 @@ class TestGeometryUtility():
         assert isinstance(geometry_obj, Function)
         # TODO: Expand this test
 
-    def test_point_from_ip(self):
-        point = GeometryUtility.point_from_ip("65.88.88.124")
-        assert point == 'SRID=4326;POINT (-73.9169 40.8056)'
+    @pytest.mark.parametrize(
+        "ip,expected",
+        [
+            ("65.88.88.124", "SRID=4326;POINT (-73.9169 40.8056)"),
+            ("127.0.0.1", None)
+        ]
+    )
+    def test_point_from_ip(self, ip, expected):
+        if expected is None:
+            assert GeometryUtility.point_from_ip(ip) is None
+        else:
+            assert GeometryUtility.point_from_ip(ip) == expected
+        
+    @pytest.mark.parametrize(
+        "bad_point_from_string_value",
+        [(None), ("No comma"), ("Not a number, -71"), ("-400,1"), ("1,400")]
+    )
+    def test_point_from_string_bad_input(self, bad_point_from_string_value):
+        assert GeometryUtility.point_from_string(bad_point_from_string_value) is None
 
-        point = GeometryUtility.point_from_ip("127.0.0.1")
-        assert point is None
-
-    def test_point_from_string(self):
-        m = GeometryUtility.point_from_string
-
-        # Lots of strings don't map to latitude/longitude.
-        assert m(None) is None
-        assert m("No comma") is None
-        assert m("Not a number, -71") is None
-        assert m("-400,1") is None
-        assert m("1,400") is None
-
-        # Here are some strings that do.
-        for coords in ("40.7769, -73.9813", "40.7769,-73.9813"):
-            assert m(coords) == 'SRID=4326;POINT (-73.9813 40.7769)'
+    @pytest.mark.parametrize(
+        "input_val,expected",
+        [
+            ("40.7769, -73.9813", "SRID=4326;POINT (-73.9813 40.7769)"),
+            ("40.7769,-73.9813", "SRID=4326;POINT (-73.9813 40.7769)"),
+        ]
+    )
+    def test_point_from_string(self, input_val, expected):
+        assert GeometryUtility.point_from_string(input_val) == expected
 
     def test_point(self):
         point = GeometryUtility.point("80", "-4")
