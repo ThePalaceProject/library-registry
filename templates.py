b7ae0ce69a758ca043fbe5a11c8f041dc0a2226c
Tim DiLauro <tim.dilauro@gmail.com>
2021-05-18 16:42:15 -0400

    Update templates for renamed admin package.

--- templates.py
+++ templates.py
@@ -4,12 +4,12 @@ admin = """
 <head>
 <title>Library Registry</title>
 <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
-<link href=\"/static/registry-admin.css\" rel="stylesheet" />
+<link href=\"/static/library-registry-admin.css\" rel="stylesheet" />
 </head>
 <body>
-  <script src=\"/static/registry-admin.js\"></script>
+  <script src=\"/static/library-registry-admin.js\"></script>
   <script>
-    var registryAdmin = new RegistryAdmin({username: \"{{ username }}\"});
+    var registryAdmin = new LibraryRegistryAdmin({username: \"{{ username }}\"});
   </script>
 </body>
 </html>
