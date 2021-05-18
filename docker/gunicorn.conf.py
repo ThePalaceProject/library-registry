b2bdeb27b313979e5a124d29c3dbce049df42c9f
Tim DiLauro <tim.dilauro@gmail.com>
2021-05-18 17:39:35 -0400

    Remove node/npm from containers and parameterize some app directories.

--- docker/gunicorn.conf.py
+++ docker/gunicorn.conf.py
@@ -1,11 +1,26 @@
+import base64
+import hashlib
 import multiprocessing
 import os
 from pathlib import Path
 
-VENV_BASE_DIR = Path("/simplye_venv")
-VENV_ACTUAL = [Path(VENV_BASE_DIR / d) for d
-               in os.listdir(VENV_BASE_DIR)
-               if d.startswith("simplye_app-") and os.path.isdir(VENV_BASE_DIR / d)][0]
+
+def _venv_name(app_home, file='Pipfile'):
+    """Compute the virtual environment name, given an app's directory."""
+    app_home = os.path.normpath(app_home)
+    filepath = os.path.join(app_home, file)
+    hash = base64.urlsafe_b64encode(hashlib.sha256(filepath.encode()).digest()[:6]).decode()[:8]
+    last_path_component = os.path.split(app_home)[-1]
+    return f'{last_path_component}-{hash}'
+
+# VENV_BASE_DIR = Path("/simplye_venv")
+# VENV_ACTUAL = [Path(VENV_BASE_DIR / d) for d
+#                in os.listdir(VENV_BASE_DIR)
+#                if d.startswith("simplye_app-") and os.path.isdir(VENV_BASE_DIR / d)][0]
+
+VENV_BASE = os.environ.get('WORKON_HOME', '/venv')
+APP_HOME = os.environ.get('LIBRARY_REGISTRY_HOME', '/apps/library-registry')
+APP_VENV = os.path.join(VENV_BASE, os.environ.get('LIBRARY_REGISTRY_VENV', _venv_name(APP_HOME)))
 
 # Shared Settings
 wsgi_app = "app:app"
@@ -16,7 +31,7 @@ limit_request_line = 4094   # max size of HTTP request line, in bytes
 limit_request_fields = 100  # max number of header fields allowed in a request
 limit_request_field_size = 8190  # allowed size of a single HTTP header field
 preload_app = False         # defer app load till after worker start
-chdir = os.environ.get("LIBREG_HOME", "/simplye_app")  # change to this dir before loading apps
+chdir = APP_HOME  # change to this dir before loading apps
 daemon = False              # Don't background the process
 user = "nginx"
 group = "nginx"
@@ -24,8 +39,8 @@ bind = ["127.0.0.1:8000"]     # listen on 8000, only on the loopback address
 workers = (2 * multiprocessing.cpu_count()) + 1
 threads = 2
 pythonpath = ",".join([
-    str(VENV_BASE_DIR / VENV_ACTUAL),
-    "/simplye_app",
+    APP_VENV,
+    APP_HOME,
 ])
 
 # Env-Specific Settings
