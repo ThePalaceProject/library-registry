011758576b9b648ff6fcf906b0148cac0119a780
Nick Ballenger <nballenger@users.noreply.github.com>
2021-05-26 16:26:41 -0400

    Get Adobe endpoints to use utf8 decoded input (#191)
    Make adobe endpoints use decoded bytestrings

--- util/xmlparser.py
+++ util/xmlparser.py
@@ -1,17 +1,51 @@
 from lxml import etree
 from io import StringIO
 
-class XMLParser(object):
 
+class XMLParser:
     """Helper functions to process XML data."""
-
+    ##### Class Constants ####################################################  # noqa: E266
     NAMESPACES = {}
 
+    ##### Public Interface / Magic Methods ###################################  # noqa: E266
+    def process_all(self, xml, xpath, namespaces=None, handler=None, parser=None):
+        if not parser:
+            parser = etree.XMLParser()
+
+        if not handler:
+            handler = self.process_one
+
+        if isinstance(xml, str):
+            root = etree.parse(StringIO(xml), parser)
+        else:
+            root = xml
+
+        for i in root.xpath(xpath, namespaces=namespaces):
+            data = handler(i, namespaces)
+            if data:
+                yield data
+
+    def process_one(self, tag, namespaces):
+        """Abstract, should be overridden by child class"""
+        return None
+
+    ##### Private Methods ####################################################  # noqa: E266
+    def _cls(self, tag_name, class_name):
+        """Return an XPath expression that will find a tag with the given CSS class."""
+        fmt_string = 'descendant-or-self::node()/%s[contains(concat(" ", normalize-space(@class), " "), " %s ")]'
+        return fmt_string % (tag_name, class_name)
+
+    ##### Properties and Getters/Setters #####################################  # noqa: E266
+
+    ##### Class Methods ######################################################  # noqa: E266
+
+    ##### Private Class Methods ##############################################  # noqa: E266
     @classmethod
     def _xpath(cls, tag, expression, namespaces=None):
+        """Wrapper to do a namespaced XPath expression."""
         if not namespaces:
             namespaces = cls.NAMESPACES
-        """Wrapper to do a namespaced XPath expression."""
+
         return tag.xpath(expression, namespaces=namespaces)
 
     @classmethod
@@ -20,44 +54,5 @@ class XMLParser(object):
         values = cls._xpath(tag, expression, namespaces=namespaces)
         if not values:
             return None
+
         return values[0]
-
-    def _cls(self, tag_name, class_name):
-        """Return an XPath expression that will find a tag with the given CSS class."""
-        return 'descendant-or-self::node()/%s[contains(concat(" ", normalize-space(@class), " "), " %s ")]' % (tag_name, class_name)
-
-    def text_of_optional_subtag(self, tag, name, namespaces=None):
-        tag = self._xpath1(tag, name, namespaces=namespaces)
-        if tag is None or tag.text is None:
-            return None
-        else:
-            return str(tag.text)
-      
-    def text_of_subtag(self, tag, name, namespaces=None):
-        return str(tag.xpath(name, namespaces=namespaces)[0].text)
-
-    def int_of_subtag(self, tag, name, namespaces=None):
-        return int(self.text_of_subtag(tag, name, namespaces=namespaces))
-
-    def int_of_optional_subtag(self, tag, name, namespaces=None):
-        v = self.text_of_optional_subtag(tag, name, namespaces=namespaces)
-        if not v:
-            return v
-        return int(v)
-
-    def process_all(self, xml, xpath, namespaces=None, handler=None, parser=None):
-        if not parser:
-            parser = etree.XMLParser()
-        if not handler:
-            handler = self.process_one
-        if isinstance(xml, str):
-            root = etree.parse(StringIO(xml), parser)
-        else:
-            root = xml
-        for i in root.xpath(xpath, namespaces=namespaces):
-            data = handler(i, namespaces)
-            if data:
-                yield data
-
-    def process_one(self, tag, namespaces):
-        return None
