9555a5d449fc395b2e549a5a68542705cbd47316
Wes Clark <wesclark@MA-DIGDV-063602.local>
2022-05-10 00:39:00 -0400

    Correct adobe route function calls

--- library_registry/drm/routes.py
+++ library_registry/drm/routes.py
@@ -12,23 +12,23 @@ drm = Blueprint(
 @drm.route('/AdobeAuth/SignIn', methods=['POST'])
 @returns_problem_detail
 def adobe_vendor_id_signin():
-    if current_app.library_registry.drm.adobe_vendor_id:
-        return current_app.library_registry.drm.adobe_vendor_id.signin_handler()
+    if current_app.library_registry.adobe_vendor_id:
+        return current_app.library_registry.adobe_vendor_id.signin_handler()
     else:
         return Response("", 404)
 
 @drm.route('/AdobeAuth/AccountInfo', methods=['POST'])
 @returns_problem_detail
 def adobe_vendor_id_accountinfo():
-    if current_app.library_registry.drm.adobe_vendor_id:
-        return current_app.library_registry.drm.adobe_vendor_id.userinfo_handler()
+    if current_app.library_registry.adobe_vendor_id:
+        return current_app.library_registry.adobe_vendor_id.userinfo_handler()
     else:
         return Response("", 404)
 
 @drm.route('/AdobeAuth/Status')
 @returns_problem_detail
 def adobe_vendor_id_status():
-    if current_app.library_registry.drm.adobe_vendor_id:
-        return current_app.library_registry.drm.adobe_vendor_id.status_handler()
+    if current_app.library_registry.adobe_vendor_id:
+        return current_app.library_registry.adobe_vendor_id.status_handler()
     else:
         return Response("", 404)
