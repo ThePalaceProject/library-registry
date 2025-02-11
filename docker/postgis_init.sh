0d52f79d21c31e87576f5f4f70bde401f8b8323e
Jonathan Green <jonathan@razzard.com>
2025-02-11 12:45:36 -0400

    Build arm containers with new github actions arm runner (PP-2139) (#765)
    * Build arm image
    * Update branch

--- docker/postgis_init.sh
+++ docker/postgis_init.sh
@@ -6,10 +6,12 @@ psql -v ON_ERROR_STOP=1 --username="$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE DATABASE simplified_registry_dev;
     CREATE USER simplified WITH PASSWORD 'simplified';
     GRANT ALL PRIVILEGES ON DATABASE simplified_registry_dev TO simplified;
+    ALTER DATABASE simplified_registry_dev OWNER TO simplified;
 
     CREATE DATABASE simplified_registry_test;
     CREATE USER simplified_test WITH PASSWORD 'simplified_test';
     GRANT ALL PRIVILEGES ON DATABASE simplified_registry_test TO simplified_test;
+    ALTER DATABASE simplified_registry_test OWNER TO simplified_test;
 
     \c simplified_registry_dev
     CREATE EXTENSION fuzzystrmatch;
@@ -18,11 +20,5 @@ psql -v ON_ERROR_STOP=1 --username="$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     \c simplified_registry_test
     CREATE EXTENSION fuzzystrmatch;
     CREATE EXTENSION postgis;
-
-    \c simplified_registry_dev postgres
-    GRANT ALL ON SCHEMA public TO simplified;
-
-    \c simplified_registry_test postgres
-    GRANT ALL ON SCHEMA public TO simplified;
 
 EOSQL
