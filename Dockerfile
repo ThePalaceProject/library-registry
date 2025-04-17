ThePalaceProject/library-registry

commit 9841faff3e78018f3ac6eb1b4855305444473878

Bump poetry version to match dependabot (#813)
## Description

Bump up the Poetry version we are using to match what dependabot uses.

CM PR: https://github.com/ThePalaceProject/circulation/pull/2403

## Motivation and Context

I've noticed some messages in our CI lately that the lock file version
may not be supported, since dependabot has moved to a 2.x version of
Poetry.

## How Has This Been Tested?

- Build in CI

## Checklist

- [X] I have updated the documentation accordingly.
- [X] All new and existing tests passed.

Dockerfile
@@ -49,7 +49,7 @@ ENV NGINX_VERSION=1.23.3
 ENV NJS_VERSION=0.7.9
 ENV PKG_RELEASE=1
 ENV SUPERVISOR_VERSION=4.2.2
-ENV POETRY_VERSION=1.5.1
+ENV POETRY_VERSION=2.1.1
 ENV POETRY_URL="https://install.python-poetry.org"
 ENV POETRY_HOME="/etc/poetry"
 # required for postgres ssl: the crt file doesn't exist
@@ -143,7 +143,7 @@ RUN set -ex \
     jpeg-dev \
     libxcb-dev \
  && cd "${LIBRARY_REGISTRY_DOCKER_HOME}" \
- && poetry install --only main,pg \
+ && poetry sync --only main,pg \
  && poetry cache clear -n --all pypi \
  && apk del --no-network .build-deps
 
@@ -170,7 +170,7 @@ ENV FLASK_ENV development
 # Install development dependancies with poetry
 RUN set -ex \
  && apk add --no-cache --virtual .build-deps build-base \
- && poetry install --no-root -E pg \
+ && poetry sync -E pg \
  && poetry cache clear -n --all pypi \
  && cd "${LIBRARY_REGISTRY_DOCKER_HOME}" \
  && apk del --no-network .build-deps

.github/workflows/lint.yml
@@ -21,7 +21,7 @@ jobs:
       - name: Install Poetry
         uses: ThePalaceProject/circulation/.github/actions/poetry@main
         with:
-          version: "1.5.1"
+          version: "2.1.1"
 
       - name: Install Pre-commit
         run: |

pyproject.toml
@@ -13,6 +13,7 @@ description = "The Palace Project library registry"
 homepage = "https://thepalaceproject.org"
 license = "Apache-2.0"
 name = "library-registry"
+package-mode = false
 readme = "README.md"
 repository = "https://github.com/ThePalaceProject/library-registry"
 version = "0"  # Version number is managed with tags in git

commit a5973a6513ff94842166d75d5b9a9b3083f01f03

Fix container failing to start no supervisord (#823)
## Description

Fix docker build.

I also switched from ENTRYPOINT to CMD to make debugging easier.

## Motivation and Context

While trying to test PP-2380 I realized that the docker build is broken.
I inadvertently broke it in
https://github.com/ThePalaceProject/library-registry/pull/813. We can't
use `poetry sync` because it uninstalls `supervisord`.

## How Has This Been Tested?

- Tested docker build locally

## Checklist

- [x] I have updated the documentation accordingly.
- [x] All new and existing tests passed.

Dockerfile
@@ -143,7 +143,7 @@ RUN set -ex \
     jpeg-dev \
     libxcb-dev \
  && cd "${LIBRARY_REGISTRY_DOCKER_HOME}" \
- && poetry sync --only main,pg \
+ && poetry install --only main,pg \
  && poetry cache clear -n --all pypi \
  && apk del --no-network .build-deps
 
@@ -152,7 +152,7 @@ COPY ./docker/nginx.conf /etc/nginx/nginx.conf
 COPY ./docker/supervisord-alpine.ini /etc/supervisord.conf
 COPY ./docker/docker-entrypoint.sh /docker-entrypoint.sh
 
-ENTRYPOINT ["/bin/sh", "-c", "/docker-entrypoint.sh"]
+CMD ["/docker-entrypoint.sh"]
 
 ##############################################################################
 
@@ -170,7 +170,7 @@ ENV FLASK_ENV development
 # Install development dependancies with poetry
 RUN set -ex \
  && apk add --no-cache --virtual .build-deps build-base \
- && poetry sync -E pg \
+ && poetry install -E pg \
  && poetry cache clear -n --all pypi \
  && cd "${LIBRARY_REGISTRY_DOCKER_HOME}" \
  && apk del --no-network .build-deps