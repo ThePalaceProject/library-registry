a6ebeb782a9396dc3a38b55720f114821b893184
Tim DiLauro <tdilauro@users.noreply.github.com>
2026-02-12 08:59:54 -0500

    Remove location-based results from /libraries feeds (PP-3587) (#855)
    ## Description
    
    Change the behavior of the `/libraries` and `/libraries/qa` OPDS feed
    endpoints so that they:
    - no longer return location-based results at the beginning of the feed
    based on a client's specified (via `_location` parameter) or inferred
    (from IP address) location; and
    - return the same feed, regardless of the clients location.
     
    ## Motivation and Context
    
    When the feeds endpoints considered location, they would return five
    results at the beginning of the feed that were determined to be
    "closest" to the client. Removing the location-based results from the
    feeds makes it possible to cache them with a CDN.
    
    [Jira PP-3587]
    
    ## How Has This Been Tested?
    
    - Tests updated and cleaned up.
    - [CI
    tests](https://github.com/ThePalaceProject/library-registry/actions/runs/21922443750)
    pass.
    
    ## Checklist
    
    - [x] I have updated the documentation accordingly.
    - [x] All new and existing tests passed.

--- app.py
+++ app.py
@@ -124,20 +124,16 @@ def confirm_resource(resource_id, secret):
 
 @app.route("/libraries")
 @compressible
-@uses_location
 @returns_problem_detail
-def libraries_opds(_location=None):
-    return app.library_registry.registry_controller.libraries_opds(location=_location)
+def libraries_opds():
+    return app.library_registry.registry_controller.libraries_opds()
 
 
 @app.route("/libraries/qa")
 @compressible
-@uses_location
 @returns_problem_detail
-def libraries_qa(_location=None):
-    return app.library_registry.registry_controller.libraries_opds(
-        location=_location, live=False
-    )
+def libraries_qa():
+    return app.library_registry.registry_controller.libraries_opds(live=False)
 
 
 @app.route("/admin/log_in", methods=["POST"])
