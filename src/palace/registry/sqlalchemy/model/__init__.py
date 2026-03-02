5fc8c87e617a861764cc5ba47f59f00d76cc3644
Tim DiLauro <tdilauro@users.noreply.github.com>
2026-03-02 17:29:07 -0500

    Use bare imports in db model module

--- src/palace/registry/sqlalchemy/model/__init__.py
+++ src/palace/registry/sqlalchemy/model/__init__.py
@@ -1,43 +1,19 @@
-"""SQLAlchemy models for Library Registry."""
+"""
+We rely on all of our sqlalchemy models being listed here, so that we can
+make sure they are all registered with the declarative base.
+This is necessary to make sure that all of our models are properly reflected in
+the database when we run migrations or create a new database.
+"""
 
-# Base setup and utilities
-# Model classes
-from palace.registry.sqlalchemy.constants import LibraryType
-from palace.registry.sqlalchemy.model.admin import Admin
-from palace.registry.sqlalchemy.model.audience import Audience, libraries_audiences
-from palace.registry.sqlalchemy.model.base import Base
-from palace.registry.sqlalchemy.model.collection_summary import CollectionSummary
-from palace.registry.sqlalchemy.model.configuration_setting import ConfigurationSetting
-from palace.registry.sqlalchemy.model.delegated_patron_identifier import (
-    DelegatedPatronIdentifier,
-    ShortClientTokenDecoder,
-)
-from palace.registry.sqlalchemy.model.external_integration import ExternalIntegration
-from palace.registry.sqlalchemy.model.hyperlink import Hyperlink
-from palace.registry.sqlalchemy.model.library import Library, LibraryAlias
-from palace.registry.sqlalchemy.model.place import Place, PlaceAlias
-from palace.registry.sqlalchemy.model.resource import Resource, Validation
-from palace.registry.sqlalchemy.model.service_area import ServiceArea
-
-__all__ = [
-    # Base and utilities
-    "Base",
-    # Models
-    "Admin",
-    "Audience",
-    "CollectionSummary",
-    "ConfigurationSetting",
-    "DelegatedPatronIdentifier",
-    "ExternalIntegration",
-    "Hyperlink",
-    "Library",
-    "LibraryAlias",
-    "LibraryType",
-    "Place",
-    "PlaceAlias",
-    "Resource",
-    "ServiceArea",
-    "ShortClientTokenDecoder",
-    "Validation",
-    "libraries_audiences",
-]
+import palace.registry.sqlalchemy.model.admin
+import palace.registry.sqlalchemy.model.audience
+import palace.registry.sqlalchemy.model.base
+import palace.registry.sqlalchemy.model.collection_summary
+import palace.registry.sqlalchemy.model.configuration_setting
+import palace.registry.sqlalchemy.model.delegated_patron_identifier
+import palace.registry.sqlalchemy.model.external_integration
+import palace.registry.sqlalchemy.model.hyperlink
+import palace.registry.sqlalchemy.model.library
+import palace.registry.sqlalchemy.model.place
+import palace.registry.sqlalchemy.model.resource
+import palace.registry.sqlalchemy.model.service_area
